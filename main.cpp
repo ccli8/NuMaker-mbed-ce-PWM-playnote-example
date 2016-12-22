0ba50c8a240e2b043aba674dbb1eb1e4884fdacc
shliu1 <shliu1@nuvoton.com>
2016-12-22 15:16:56 +0800

    Support M453, target is numaker_pfm_m453

--- main.cpp
+++ main.cpp
@@ -16,7 +16,12 @@
 #define _A6   1760 // A6      = 1760.00Hz
 #define _B6   1976 // B6      = 1975.53Hz
 
+#if defined(TARGET_NUMAKER_PFM_NUC472)
 PwmOut pwm0(PF_9); // PWM0 pin 
+#elif defined(TARGET_NUMAKER_PFM_M453)
+PwmOut pwm0(PC_6); // PWM0_pin
+#endif
+
 
 int main()
 {
